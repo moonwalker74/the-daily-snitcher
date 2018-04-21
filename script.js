f56beed0e05e6d6ac2516c9d6db1251ef988c201
marnie <marine.bernard7@gmail.com>
2018-04-21 13:10:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-
 $('a.changecolor').click(function() {
     var house = $(this).attr("id"); 
     $('#paper').removeClass();
     $('#paper').addClass($(this).attr("id"));
+    return false;
+});
+
+$('.readmore a').click(function() {
+  var article = '#article'+$(this).attr("id");
+    $('#content').fadeOut(150);
+     $(article).delay(151).fadeIn(150);
+    return false;
+});
+
+$('.frontpage a').click(function() {
+  var article = '#article'+$(this).attr("id");
+    $(article).fadeOut(150);
+     $('#content').delay(151).fadeIn(150);
     return false;
 });
